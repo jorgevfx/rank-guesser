6831f3b94c58ab042ed7e9adb9ae0028fdf2f135
george <jorgeoah.20@outlook.com>
2023-10-03 19:01:57 -0500

    :pencil2: Fix: Some typos, colors and text sizes

--- src/components/FinalResults.vue
+++ src/components/FinalResults.vue
@@ -7,7 +7,7 @@ import AnimatedCheck from "@/components/icons/AnimatedCheck.vue";
     <AnimatedCheck color="rgb(42 53 73)"/>
     <h1>Final Results</h1>
     <p>Thanks for playing! You've seen all the selected clips for today</p>
-    <p>Next clips will be available tomorrow at 12:00 PM</p>
+    <p>Next clips will be available tomorrow at 12:00 AM</p>
   </div>
 </template>
 
@@ -16,23 +16,24 @@ import AnimatedCheck from "@/components/icons/AnimatedCheck.vue";
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-top: 32px;
   }
 
   .results__container h1, p{
     font-family: Poppins, sans-serif !important;
     text-align: center;
-    color: #cbd7e6;
+    color: #e3ecf6;
   }
 
   .results__container h1 {
-    font-size: clamp(32px, 5vw, 38px);
+    font-size: clamp(34px, 5vw, 42px);
     font-weight: 600;
     margin-bottom: 0.5em;
     letter-spacing: 2px;
   }
 
   .results__container p {
-    font-size: clamp(14px, 5vw, 22px);
+    font-size: clamp(12px, 5vw, 20px);
     font-weight: 200;
     margin-bottom: 0.5em;
   }
