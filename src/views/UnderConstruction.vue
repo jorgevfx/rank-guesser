014f6b69169c387b53ce1bb4a56f4b07ceb4c729
george <jorgeoah.20@outlook.com>
2023-10-03 19:01:17 -0500

    :white_check_mark: Add: UnderConstruction page

---
+++ src/views/UnderConstruction.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+import ConstructionCrane from "@/components/icons/ConstructionCrane.vue";
+</script>
+
+<template>
+  <div class="container">
+    <ConstructionCrane/>
+    <h1>Page under construction</h1>
+  </div>
+
+</template>
+
+<style scoped>
+  .container {
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .container h1 {
+    font-family: Poppins, sans-serif !important;
+    text-align: center;
+    font-weight: 700;
+    font-size: clamp(36px, 5vw, 48px);
+  }
+</style>
